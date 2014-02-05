e6580bcdf186c3c84b6b767d992cbb561b06e0cc
Soheil <ysoheil@gmail.com>
2014-02-04 17:38:59 -0800

    fix ruby path

--- dmg-me.sh
+++ dmg-me.sh
@@ -1,1 +1,2 @@
-hdiutil create Ruber.dmg -volname "Ruber" -fs HFS+ -srcfolder $1
+\rm Ruber.dmg
+hdiutil create Ruber.dmg -volname "Ruber" -fs HFS+ -srcfolder /Users/soheil/Library/Developer/Xcode/DerivedData/Popup-baiatrsspvsqakefhydhtqvarbpe/Build/Products/Debug/Ruber.app
